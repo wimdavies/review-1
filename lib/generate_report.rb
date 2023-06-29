9cfecbeb7e430dd703b3527ab898a67f3e333037
wimdavies <william.johnston.davies@gmail.com>
2023-06-29 11:35:51 +0100

    before out of time: testdrove up multi-type context; after: completed TDDing that context, for my own closure

--- lib/generate_report.rb
+++ lib/generate_report.rb
@@ -1,3 +1,25 @@
 def generate_report(string)
-  'No results have been given'
+  return 'No results have been given' if string == ""
+
+  green_count = 0
+  amber_count = 0
+  red_count = 0
+
+  string.split(', ').each do |result|
+    case result
+    when 'Green'
+      green_count += 1
+    when 'Amber'
+      amber_count += 1
+    when 'Red'
+      red_count += 1
+    end
+  end
+
+  report_array = []
+  report_array << "Green: #{green_count}" if green_count > 0
+  report_array << "Amber: #{amber_count}" if amber_count > 0
+  report_array << "Red: #{red_count}" if red_count > 0
+
+  report = report_array.join("\n")
 end
--- spec/generate_report_spec.rb
+++ spec/generate_report_spec.rb
@@ -6,4 +6,30 @@ RSpec.describe 'generate_report' do
       expect(generate_report('')).to eq 'No results have been given'
     end
   end
+
+  context 'returns desired format for single examples of each result type' do
+    it 'green' do
+      expect(generate_report('Green')).to eq 'Green: 1'
+    end
+
+    it 'amber' do
+      expect(generate_report('Amber')).to eq 'Amber: 1'
+    end
+
+    it 'red' do
+      expect(generate_report('Red')).to eq 'Red: 1'
+    end
+  end
+
+  context 'processes single examples of more than one type' do
+    it 'when in output order' do
+      result = generate_report('Green, Amber, Red')
+      expect(result).to eq "Green: 1\nAmber: 1\nRed: 1"
+    end
+
+    it 'when not in output order' do
+      result = generate_report('Amber, Green, Red')
+      expect(result).to eq "Green: 1\nAmber: 1\nRed: 1"
+    end
+  end
 end
